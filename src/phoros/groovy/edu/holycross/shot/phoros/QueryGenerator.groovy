ff12d03dc3fd0dafffbe6c344df0d5491e9f08c1
Neel Smith <dnsmith.neel@gmail.com>
2014-02-25 05:15:01 -0500

    Implement placecsv.

--- src/phoros/groovy/edu/holycross/shot/phoros/QueryGenerator.groovy
+++ src/phoros/groovy/edu/holycross/shot/phoros/QueryGenerator.groovy
@@ -1,11 +1,26 @@
 package edu.holycross.shot.phoros
 
+
 class QueryGenerator {
 
   QueryGenerator() {}
 
-  String msg() {
-    return "Yo mama."
+
+
+  String siteQuery(String urnStr) {
+    return """SELECT ?lab ?lon ?lat ?payrec ?yr ?obs  ?txt WHERE {
+?s <http://www.homermultitext.org/hmt/citedata/places_Lon> ?lon .
+?s <http://www.homermultitext.org/hmt/citedata/places_Lat> ?lat .
+?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#label>  ?lab .
+?s <http://shot.holycross.edu/phoros/rdf/paid>  ?payrec .
+?payrec <http://www.homermultitext.org/hmt/citedata/payrec_Year> ?yr .
+?payrec <http://www.homermultitext.org/hmt/citedata/payrec_Obols>  ?obs .
+?payrec <http://www.homermultitext.org/hmt/citedata/payrec_TextPassage> ?txt .
+
+FILTER (str(?s) = "${urnStr}") 
+}
+ORDER BY ?yr 
+"""
   }
 
 }
