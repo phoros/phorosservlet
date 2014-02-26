c27e163f14e104e70563c81a2f0b2be7f3910f27
Neel Smith <dnsmith.neel@gmail.com>
2014-02-26 05:18:02 -0500

    Update presentation.

--- src/phoros/groovy/edu/holycross/shot/phoros/QueryGenerator.groovy
+++ src/phoros/groovy/edu/holycross/shot/phoros/QueryGenerator.groovy
@@ -1,21 +1,54 @@
 package edu.holycross.shot.phoros
 
+//import edu.harvard.chs.cite.CtsUrn
 
 class QueryGenerator {
 
   QueryGenerator() {}
 
 
+  /**
+   * @param urnStr CTS URN of a *diplomatic* edition.
+   */
+  String imgsForDoc(String urnStr) {
+    String queryStr = urnStr
+    /*
+    CtsUrn urn
+
+    try {
+      urn = new CtsUrn(urnStr)
+    } catch (Exception  e) {
+      throw e
+    }
+    String queryStr =  urn.getUrnWithoutPassage()
+    if (urn.getVersion() != "hc1") {
+      queryStr = "urn:cts:${urn.getCtsNs()}:${urn.getTextGroup()}.${urn.getWork()}.hc1"
+    }
+    */
+    return """
+SELECT ?stone ?doc ?defimg ?img WHERE {
+
+?stone <http://www.homermultitext.org/hmt/citedata/documents_CtsUrn>  ?doc .
+?stone <http://www.homermultitext.org/hmt/citedata/documents_DefaultImg> ?defimg .
+?stone <http://www.homermultitext.org/cite/rdf/illustratedBy>   ?img .
+FILTER (str(?doc)= "${queryStr}")
+}"""
+  }
+
 
   String siteQuery(String urnStr) {
     return """SELECT ?lab ?lon ?lat ?payrec ?yr ?obs  ?txt WHERE {
-?s <http://www.homermultitext.org/hmt/citedata/places_Lon> ?lon .
-?s <http://www.homermultitext.org/hmt/citedata/places_Lat> ?lat .
+
 ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#label>  ?lab .
 ?s <http://shot.holycross.edu/phoros/rdf/paid>  ?payrec .
 ?payrec <http://www.homermultitext.org/hmt/citedata/payrec_Year> ?yr .
 ?payrec <http://www.homermultitext.org/hmt/citedata/payrec_Obols>  ?obs .
 ?payrec <http://www.homermultitext.org/hmt/citedata/payrec_TextPassage> ?txt .
+
+OPTIONAL {
+?s <http://www.homermultitext.org/hmt/citedata/places_Lon> ?lon .
+?s <http://www.homermultitext.org/hmt/citedata/places_Lat> ?lat .
+}
 
 FILTER (str(?s) = "${urnStr}") 
 }
