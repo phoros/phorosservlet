4f5f6a103c606e15d31a467bee4609b3b3afe115
Neel Smith <dnsmith.neel@gmail.com>
2014-03-03 10:26:08 -0500

    Improve query for all phoros.

--- src/phoros/groovy/edu/holycross/shot/phoros/QueryGenerator.groovy
+++ src/phoros/groovy/edu/holycross/shot/phoros/QueryGenerator.groovy
@@ -46,6 +46,27 @@ FILTER (str(?doc)= "${queryStr}")
 }"""
   }
 
+  String phorosQuery() {
+    return """SELECT ?urn ?lab ?lon ?lat ?payrec ?yr ?obs  ?txt WHERE {
+
+?urn <http://www.w3.org/1999/02/22-rdf-syntax-ns#label>  ?lab .
+?urn <http://shot.holycross.edu/phoros/rdf/paid>  ?payrec .
+?payrec <http://www.homermultitext.org/hmt/citedata/payrec_Year> ?yr .
+?payrec <http://www.homermultitext.org/hmt/citedata/payrec_Obols>  ?obs .
+?payrec <http://www.homermultitext.org/hmt/citedata/payrec_TextPassage> ?txt .
+
+OPTIONAL {
+?urn <http://www.homermultitext.org/hmt/citedata/places_Lon> ?lon .
+?urn <http://www.homermultitext.org/hmt/citedata/places_Lat> ?lat .
+}
+
+FILTER (!regex(str(?lab), "^urn:.+\$" ) ) .
+
+}
+ORDER BY ?urn 
+"""
+  }
+
 
   String siteQuery(String urnStr) {
     return """SELECT ?lab ?lon ?lat ?payrec ?yr ?obs  ?txt WHERE {
@@ -66,5 +87,8 @@ FILTER (str(?s) = "${urnStr}")
 ORDER BY ?yr 
 """
   }
+
+
+
 
 }
